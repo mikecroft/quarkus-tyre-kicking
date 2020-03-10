0d2d18996ca47424796a4bbe48482a67c6f6a1a9
Mike Croft <backtotheoldhouse@gmail.com>
2020-03-10 17:24:18 +0000

    add a test of snapshot container-image features

---
+++ quarkus-knative/src/test/java/io/mikecroft/NativeDuckResourceIT.java
@@ -0,0 +1,9 @@
+package io.mikecroft;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeDuckResourceIT extends DuckResourceTest {
+
+    // Execute the same tests but in native mode.
+}
